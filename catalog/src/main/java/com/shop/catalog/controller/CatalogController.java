ba4a6ddb50d035d3b76a64816ff380e18901ec2e
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2024-11-01 21:20:14 -0300

    Creating catalog page

--- catalog/src/main/java/com/shop/catalog/controller/CatalogController.java
+++ catalog/src/main/java/com/shop/catalog/controller/CatalogController.java
@@ -3,18 +3,20 @@ package com.shop.catalog.controller;
 import com.shop.catalog.model.Products;
 import com.shop.catalog.service.ICatalogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class CatalogController {
 
     private final ICatalogService catalogService;
 
-    @GetMapping("/list")
-    public ResponseEntity<Products> getProdutos() {
-        return ResponseEntity.ok(catalogService.getProdutos());
+    @GetMapping
+    public String getProdutos(Model model) {
+        Products products = catalogService.getProdutos();
+        model.addAttribute("products", products);
+        return "catalog";
     }
 }
