675623cc232911964fd4700457eeaf7df9080d43
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2024-11-11 23:23:10 -0300

    Custom metric for the amount of payments

--- payment/src/main/java/com/shop/payment/service/PaymentService.java
+++ payment/src/main/java/com/shop/payment/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.shop.payment.service;
 
 import com.shop.payment.model.Order;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +13,8 @@ public class PaymentService implements IPaymentService {
 
     private final RabbitTemplate rabbitTemplate;
 
+    private final MeterRegistry meterRegistry;
+
     @Value("${exchange.payment.name}")
     private String exchangePaymentName;
 
@@ -19,5 +22,10 @@ public class PaymentService implements IPaymentService {
     public void acceptOrder(Order order) {
         order.setStatus("aprovado");
         rabbitTemplate.convertAndSend(exchangePaymentName, "", order);
+        incrementMetric();
+    }
+
+    private void incrementMetric() {
+        meterRegistry.counter("payment_increment").increment();
     }
 }
